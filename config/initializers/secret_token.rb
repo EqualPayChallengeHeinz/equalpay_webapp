86bfd1b8fda2bd7b0ad3a50d5abb75f3186f8c83
Narendra Chennamsetty <narench@gmail.com>
2012-03-07 22:57:07 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Equalpay::Application.config.secret_token = 'b09484bc03a2d123e2b744de19feb1c8db89b029372427a4fe30925d4b20273fda3f7d365a0224b9410506d84c4eeecd65f0d619d0d83a68e32556c2ea0b680d'
